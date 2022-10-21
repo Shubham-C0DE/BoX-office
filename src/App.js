cfa063082d66468bbfce50fd9250d5a1e49b8c6a
Shubham-C0DE <shubhamsagar616@gmail.com>
2022-10-21 12:55:18 +0530

    added page layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Starred from './pages/Starred';
 import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <Navs />
+   
 
       <Switch>
         <Route exact path="/">
@@ -24,7 +22,7 @@ function App() {
           </div>
         </Route>
       </Switch>
-    </div>
+  
   );
 }
 
