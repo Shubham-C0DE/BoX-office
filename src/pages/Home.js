a6b949baad72228d9f95bfe202feaeb4e1b35579
Shubham-C0DE <shubhamsagar616@gmail.com>
2022-10-21 20:01:38 +0530

    added search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowSearch = searchOption === 'shows'
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
+  };
+    
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.key === 'Enter')
+      // eslint-disable-next-line no-use-before-define
+      onSearch();
+  };
+const onRadioChange = (ev) => {
+setSearchOption(ev.target.value)
+}
+
+
+
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Result</div>;
+    }
+
+    if (results && results.length > 0) {
+      return results[0].show 
+      ? results.map(item => (
+        <div key={item.show.id}>{item.show.name}</div>
+      )) : results.map(item => (
+        <div key={item.person.id}>{item.person.name}</div>
+      ))
+         
+    }
+
+    return null;
+  };
+
   return (
     <MainPageLayout>
-        This Is Home
+      <input
+        type="text"
+        placeholder='search here something'
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
+<div>
+ <label htmlFor='shows-search'>
+  Shows
+  <input id='shows-search' type= "radio" value="shows" checked={isShowSearch}  onChange={onRadioChange}  />
+ </label>
+
+
+ <label htmlFor='actors-search'>
+  Actors
+  <input id='actors-search' type= "radio" value="people" checked={!isShowSearch} onChange={onRadioChange} />
+ </label>
+
+
+
+</div>
+
+
+      <button type="button" onClick={onSearch} onKeyDown={onKeyDown}>
+        search
+      </button>
+      {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
 export default Home;
