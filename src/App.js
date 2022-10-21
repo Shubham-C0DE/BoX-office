b6c4f3e7bd0ceeec1f487224f979db18a17b54ec
Shubham-C0DE <shubhamsagar616@gmail.com>
2022-10-21 11:33:48 +0530

    added react-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <Route exact path="/">This Is Our Home Page</Route>
+
+      <Route exact path="/starred">This Is Not Our Home Page</Route>
+
+      <Route>404 Page Error NOT FOUND!!!</Route>
+    </Switch>
   );
 }
 
